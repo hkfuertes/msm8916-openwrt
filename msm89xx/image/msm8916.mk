0c9ef836f6bda48d02c3374d5cad9ee522593583
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-03 00:12:49 +0200

    changes

--- msm89xx/image/msm8916.mk
+++ msm89xx/image/msm8916.mk
@@ -5,15 +5,20 @@ ifeq ($(SUBTARGET),msm8916)
 define Build/generate-gpt-squashfs
     $(TOPDIR)/target/linux/$(BOARD)/image/generate_gpt.sh \
         "$(dir $@)" \
-        $(TOPDIR)/target/linux/$(BOARD)/image/gpt-squashfs.table \
+        $(TOPDIR)/target/linux/$(BOARD)/image/tables/gpt-squashfs.table \
         "$(notdir $@)"
 endef
 
 define Build/generate-gpt-ext4
     $(TOPDIR)/target/linux/$(BOARD)/image/generate_gpt.sh \
         "$(dir $@)" \
-        $(TOPDIR)/target/linux/$(BOARD)/image/gpt-ext4.table \
+        $(TOPDIR)/target/linux/$(BOARD)/image/tables/gpt-ext4.table \
         "$(notdir $@)"
+endef
+
+define Build/install-flasher
+    $(CP) $(TOPDIR)/target/linux/$(BOARD)/image/flash.sh $@
+    chmod +x $@
 endef
 
 define Device/msm8916
@@ -25,19 +30,21 @@ define Device/yiming-uz801v3
   $(Device/msm8916)
   DEVICE_VENDOR := YiMing
   DEVICE_MODEL := uz801v3
-  DEVICE_PACKAGES := uz801-tweaks wpad-basic-wolfssl msm-firmware-dumper rmtfs rootfs-resizer
+  DEVICE_PACKAGES := uz801-tweaks wpad-basic-wolfssl msm-firmware-dumper rmtfs rootfs-resizer rootfs-data-formatter
   
   ifdef CONFIG_TARGET_ROOTFS_SQUASHFS
     IMAGE/system.img := append-rootfs | append-metadata
-    ARTIFACTS += squashfs_gpt_both0.bin squashfs_rootfs_data.img
+    ARTIFACTS += squashfs_gpt_both0.bin squashfs_rootfs_data.img flash.sh
     ARTIFACT/squashfs_gpt_both0.bin := generate-gpt-squashfs
     ARTIFACT/squashfs_rootfs_data.img := generate-gpt-squashfs
+    ARTIFACT/flash.sh := install-flasher
   endif
   
   ifdef CONFIG_TARGET_ROOTFS_EXT4FS
     IMAGE/system.img := append-rootfs | append-metadata | sparse-img
-    ARTIFACTS += ext4_gpt_both0.bin
+    ARTIFACTS += ext4_gpt_both0.bin flash.sh
     ARTIFACT/ext4_gpt_both0.bin := generate-gpt-ext4
+    ARTIFACT/flash.sh := install-flasher
   endif
 endef
 TARGET_DEVICES += yiming-uz801v3
